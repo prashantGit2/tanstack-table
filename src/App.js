f2069e1f0ece304b11f1909cd837a58dc06a8282
Prashant Sharma <87496363+prashantGit2@users.noreply.github.com>
2022-11-10 18:20:02 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import Table from './components/table'
+import './App.css'
+const App = () => {
+  return (
+    <>
+    <div className='App'>
+      {/* <a href='#last' >last</a> */}
+      <Table />
+    </div>
+    {/* <div style={{display:"flex",flexDirection:"column"}}>
+      <div id="first" className='vh30 center-items bg-blue'>this should be downside #first</div>
+      <div className='vh30 center-items bg-red'>this should be downside</div>
+      <div className='vh30 center-items bg-blue'>this should be downside</div>
+      <div className='vh30 center-items bg-red'>this should be downside</div>
+      <div id="m" className='vh30 center-items bg-blue'>this should be downside</div>
+      <div className='vh30 center-items bg-red'>this should be downside</div>
+      <div className='vh30 center-items bg-blue'>this should be downside</div>
+      <div className='vh30 center-items bg-red'>this should be downside</div>
+      <div className='vh30 center-items bg-blue'>this should be downside</div>
+      <div id="last" className='vh30 center-items bg-red'>this should be downside #last</div>
+      
+      
+    </div> */}
+    </>
+  )
+}
+
+export default App
